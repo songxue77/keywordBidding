5cb491b1759a67203792ae0591cde730737b8ff0
심설송 <snowbrightly@hotmail.com>
2020-07-30 22:34:01 +0900

    fea(project): 프로젝트 초기화

---
+++ src/main/java/songxue77/keywordBidding/KeywordBiddingApplication.java
@@ -0,0 +1,13 @@
+package songxue77.keywordBidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordBiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordBiddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/songxue77/keywordBidding/KeywordBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package songxue77.keywordBidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
