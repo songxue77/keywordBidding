e386575d35c695d95116e61419352cbcd5ead484
심설송 <snowbrightly@hotmail.com>
2020-08-11 23:09:23 +0900

    fea(member로직): member 레파지토리

---
+++ src/main/java/songxue77/keywordBidding/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package songxue77.keywordBidding.repository;
+
+import songxue77.keywordBidding.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
